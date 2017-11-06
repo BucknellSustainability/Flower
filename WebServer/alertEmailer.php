b9067e6c1afda9ad82d7a53dbb51b5765bd22ffb
bdm015 <bdm015@bucknell.edu>
2017-11-05 20:18:39 -0500

    Changed DB query to grab unhandled alerts

---
+++ WebServer/alertEmailer.php
@@ -0,0 +1,57 @@
+<?php
+
+$link = mysqli_connect("digitalgreens.cixglou4nxxh.us-east-1.rds.amazonaws.com", "jvoves", "digitalgreens", "energyhill");
+
+//make sure connection is established
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+//get all undhandled alerts
+$query = "SELECT * FROM alerts WHERE handled != 1;";
+
+//put al sites in an array and output as json
+if ($result = mysqli_query($link, $query)) {
+    $rows = Array();
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+    echo json_encode($rows);
+    mysqli_free_result($result);
+}
+
+mysqli_close($link);
+
+
+
+
+
+
+
+
+
+
+/*
+    //open connection to mysql db
+    $connection = mysqli_connect("digitalgreens.cixglou4nxxh.us-east-1.rds.amazonaws.com","jvoves","digitalgreens");
+
+    $sql = "select name from site";
+    $result = mysqli_query($connection, $sql);
+
+
+    $emparray = array();
+    while($row = mysqli_fetch_assoc($result))
+    {
+        $emparray[] = $row;
+    }
+
+
+    echo json_encode($emparray);
+
+    //close the db connection
+    mysqli_close($connection);
+    */
+
+
+?>
