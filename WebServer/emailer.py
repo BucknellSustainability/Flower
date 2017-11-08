df3a77613240655fba0f88ea413789243e0ea894
bdm015 <bdm015@bucknell.edu>
2017-11-08 12:56:42 -0500

    Added code to get all rows

--- WebServer/emailer.py
+++ WebServer/emailer.py
@@ -3,21 +3,30 @@
 import smtplib
 import MySQLdb
 
-# Open database connection
-db = MySQLdb.connect("digitalgreens.cixglou4nxxh.us-east-1.rds.amazonaws.com", "jvoves", "digitalgreens", "energyhill")
+def main():
+    # Open database connection
+    db = MySQLdb.connect("digitalgreens.cixglou4nxxh.us-east-1.rds.amazonaws.com", "jvoves", "digitalgreens", "energyhill")
 
-# prepare a cursor object using cursor() method
-cursor = db.cursor()
+    # prepare a cursor object using cursor() method
+    cursor = db.cursor()
 
-# execute SQL query using execute() method.
-cursor.execute("SELECT VERSION()")
+    sql = "SELECT * FROM alerts WHERE handled != 1;"
 
-# Fetch a single row using fetchone() method.
-data = cursor.fetchone()
-print "Database version : %s " % data
+    unhandled = None
 
-# disconnect from server
-db.close()
+    try:
+        # Execute the SQL command
+        cursor.execute(sql)
+        # Fetch all the rows in a list of lists.
+        unhandled = cursor.fetchall()
+    except:
+        print "Error: unable to fecth data"
+
+    for row in unhandled:
+        print(row)
+
+    # disconnect from server
+    db.close()
 
 
 def sendEnergyHillEmail(receiver, body):
@@ -32,8 +41,12 @@ def sendEnergyHillEmail(receiver, body):
     """.format(sender, receivers[0])
 
     try:
-       smtpObj = smtplib.SMTP('smtp.bucknell.edu')
-       smtpObj.sendmail(sender, receivers, message)
-       print "Successfully sent email"
+        smtpObj = smtplib.SMTP('smtp.bucknell.edu')
+        smtpObj.sendmail(sender, receivers, message)
+        print "Successfully sent email"
     except SMTPException:
-       print "Error: unable to send email"
+        print "Error: unable to send email"
+
+
+if __name__ == "__main__":
+    main()
