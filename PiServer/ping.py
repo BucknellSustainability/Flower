e683a94d100b33ffb97fbadc73bf8fa159bf0f6d
Phlosioneer <mattmdrr2@gmail.com>
2017-11-09 20:59:44 -0500

    IT'S WORKING!

--- PiServer/ping.py
+++ PiServer/ping.py
@@ -13,13 +13,45 @@ import pymysql
 # sudo pip3 install pymysql
 # That should fix it.
 
+import serial
+# To install pyserial:
+# sudo apt-get install python3-pip
+# sudo pip3 install pyserial
+
 
 connection = pymysql.connect(host="digitalgreens.cixglou4nxxh.us-east-1.rds.amazonaws.com", user="jvoves", password="digitalgreens", db="energyhill")
 
-with connection.cursor() as cursor:
-	cursor.execute("SELECT * FROM data;")
-	data = cursor.fetchall()
+# TODO: Detect which tty port to use. Currently hardcoded for the arduino micro.
+with serial.Serial("/dev/ttyUSB0", 115200, timeout=5) as arduino:
+	data = arduino.readline()
+
+	print("discarding: ")
 	print(data)
+
+	while (arduino.is_open):
+		# Get the next line of input from the arduino. It's in the form of a
+		# binary string, with /r/n characters at the end.
+		data = arduino.readline()
+		
+		# Chop off the last two characters (/r/n)
+		data = data[0:-2];
+	
+		# Convert the string from binary to utf-8.
+		data = data.decode("ascii")
+
+		# Push the data to the database.
+		with connection.cursor() as cursor:
+			# TODO: Avoid SQL injection.
+			# TODO: Do more things than push a value.
+			command = "INSERT INTO data (sensorId, value) VALUES (1, " + data + ");"
+			print(command)
+			cursor.execute(command)
+	
+			# Send the data to the server.
+			connection.commit()
+
+			# Print the data that we just sent.
+			print("sent: " + data)
 
 
 
