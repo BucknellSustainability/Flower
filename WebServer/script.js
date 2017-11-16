328eca6de79d7897c36eaf3f605d8952b71a1dec
bdm015 <bdm015@bucknell.edu>
2017-11-15 21:26:33 -0500

    Cleaned up most of script.js, still some lint errors

--- WebServer/script.js
+++ WebServer/script.js
@@ -1,78 +1,88 @@
 // This example displays a marker at the center of Australia.
 // When the user clicks the marker, an info window opens.
-       GOOGLE_MAPS_KEY = 'AIzaSyDhCHwOpcFGA9t6yMul0XCEhhg2mKyJ5mM';
-       var url = 'https://maps.googleapis.com/maps/api/js?key=' + GOOGLE_MAPS_KEY + '&callback=initMap';
-       var JSElement = document.createElement('script');
-           JSElement.src = url;
-           JSElement.onload = OnceLoaded;
-           document.getElementsByTagName('body')[0].appendChild(JSElement);
+GOOGLE_MAPS_KEY = 'AIzaSyDhCHwOpcFGA9t6yMul0XCEhhg2mKyJ5mM';
+url = 'https://maps.googleapis.com/maps/api/js?key=' + GOOGLE_MAPS_KEY;
+JSElement = document.createElement('script');
+    JSElement.src = url;
+    JSElement.onload = initMap;
+    document.getElementsByTagName('body')[0].appendChild(JSElement);
 
-function OnceLoaded() {
-    // Once loaded.. load other JS or CSS or call objects of version.js
-}
-
-// Reads sites from amazon rds
+/**
+Reads sites from amazon rds
+@return {array} sites
+*/
 function readSites() {
-let sites = [];
-$.ajax({
-  type: 'GET',
-  url: 'api.php',
-  success: function(json) {
-    $.each(JSON.parse(json), function(key, value) {
-      sites.push(value); // value = json object for site
+    sites = [];
+    $.ajax({
+        type: 'GET',
+        url: 'api.php',
+        success: function(json) {
+            $.each(JSON.parse(json), function(key, value) {
+                sites.push(value); // value = json object for site
+            });
+            console.log('Sucessfully Read from DB');
+        },
+        error: function() {
+            alert('Error Reading from DB');
+        },
+        async: false, // important. Makes sure init map waits till this is done.
     });
-    console.log('Sucessfully Read from DB');
-  },
-  error: function() {
-    alert('Error Reading from DB');
-  },
-  async: false,  // important. Makes sure init map waits till this is done.
-  });
-  return sites;
+    return sites;
 }
 
+/**
+    Initialized Google Map
+*/
 function initMap() {
     // get sites from db
-    let siteList = readSites(function() {});
+    siteList = readSites(function() {});
 
     console.log('Initializing Map');
-    let energyHill = {lat: 40.950409, lng: -76.882382}; // Energy Hill: 40.950409, -76.882382
-    let map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 16,
-      center: energyHill,
+    // Energy Hill: 40.950409, -76.882382
+    energyHill = {lat: 40.950409, lng: -76.882382};
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 16,
+        center: energyHill,
     }); // define map & starting location
 
-    let markers = []; // array of google map markers
-    let contents = []; // content for the markers
-    let iWindows = []; // windows to hold content for onclick
+    markers = []; // array of google map markers
+    contents = []; // content for the markers
+    iWindows = []; // windows to hold content for onclick
 
     // iterate through sites in the db
-    for (let site in siteList) {
-        let currSite = siteList[site];
+    for (site in siteList) {
+        currSite = siteList[site];
 
-        let location = {lat: parseFloat(currSite.latitude), lng: parseFloat(currSite.longitude)};
+        var location = {
+            lat: parseFloat(currSite.latitude),
+            lng: parseFloat(currSite.longitude),
+        };
+
         contents[site] = '<div id="content">'+
-          '<div id="siteNotice">'+'</div>'+'<h1 id="firstHeading" class="firstHeading">'+ currSite.name + '</h1>'+
-          '<div id="bodyContent">' + '<p>' + currSite.description +'</p>' +
-          '<p>Site: <a href=' + currSite.link+ '>Current ' + currSite.name + ' Projects</a></p>'+
-          '</div>'+
-          '</div>';
+            '<div id="siteNotice">'+'</div>'+
+            '<h1 id="firstHeading" class="firstHeading">'+ currSite.name +
+            '</h1>'+
+            '<div id="bodyContent">' + '<p>' + currSite.description +'</p>' +
+            '<p>Site: <a href=' + currSite.link+ '>Current ' + currSite.name +
+            ' Projects</a></p>'+
+            '</div>'+
+            '</div>';
 
         markers[site] = new google.maps.Marker({
-          position: location,
-          map: map,
-          title: 'Energy Hill',
-          animation: google.maps.Animation.DROP,
+            position: location,
+            map: map,
+            title: 'Energy Hill',
+            animation: google.maps.Animation.DROP,
         });
         markers[site].index = site;
 
         iWindows[site] = new google.maps.InfoWindow({
-          content: contents[site],
-          pixelOffset: new google.maps.Size(0, -20),
+            content: contents[site],
+            pixelOffset: new google.maps.Size(0, -20),
         });
 
         google.maps.event.addListener(markers[site], 'click', function() {
-            for (let i in iWindows) {
+            for (i in iWindows) {
                 iWindows[i].close();
                 markers[i].setAnimation(null);
             }
@@ -80,13 +90,13 @@ function initMap() {
             map.panTo(markers[this.index].getPosition());
             markers[this.index].setAnimation(google.maps.Animation.BOUNCE);
         });
-  }
-
-  // close info windows on click off
-  google.maps.event.addListener(map, 'click', function() {
-    for (let i in iWindows) {
-      iWindows[i].close();
-      markers[i].setAnimation(null);
     }
-  });
+
+    // close info windows on click off
+    google.maps.event.addListener(map, 'click', function() {
+        for (i in iWindows) {
+            iWindows[i].close();
+            markers[i].setAnimation(null);
+        }
+    });
 }
