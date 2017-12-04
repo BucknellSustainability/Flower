2ca3c427690b998a6bf4b33b6d8eab19069c953f
Linux Server <energyhill@bucknell.edu>
2017-12-03 20:13:37 -0500

    Revert "Merge pull request #151 from BucknellSustainability/124-js-linter"
    This reverts commit 8b05ddf5696398c134ad2b725f0b1048822883a2, reversing
    changes made to 81206bbfc1b6fc4b370275a10c1d1fa69555aa23.

--- WebServer/script.js
+++ WebServer/script.js
@@ -1,73 +1,72 @@
-/* eslint guard-for-in: "warn" */
-
 // This example displays a marker at the center of Australia.
 // When the user clicks the marker, an info window opens.
-GOOGLE_MAPS_KEY = 'AIzaSyDhCHwOpcFGA9t6yMul0XCEhhg2mKyJ5mM';
-url = 'https://maps.googleapis.com/maps/api/js?key=' + GOOGLE_MAPS_KEY;
-JSElement = document.createElement('script');
-    JSElement.src = url;
-    JSElement.onload = initMap;
-    document.getElementsByTagName('body')[0].appendChild(JSElement);
+       GOOGLE_MAPS_KEY = "AIzaSyDhCHwOpcFGA9t6yMul0XCEhhg2mKyJ5mM";
+       var url = "https://maps.googleapis.com/maps/api/js?key=" + GOOGLE_MAPS_KEY + "&callback=initMap";
+       var JSElement = document.createElement('script');
+           JSElement.src = url;
+           JSElement.onload = OnceLoaded;
+           document.getElementsByTagName('body')[0].appendChild(JSElement);
 
-/**
- * Initializes the Google Map
- */
+function OnceLoaded() {
+    // Once loaded.. load other JS or CSS or call objects of version.js
+}
+
 function initMap() {
-    // get sites from db
-    siteList = readDB('site', '', '*');
+    //get sites from db
+    var siteList = readDB("site","","*");
 
-    markers = []; // array of google map markers
-    contents = []; // content for the markers
-    iWindows = []; // windows to hold content for onclick
+    console.log("Initializing Map");
+    var energyHill = {lat: 40.950409, lng: -76.882382}; //Energy Hill: 40.950409, -76.882382
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 16,
+      center: energyHill
+    }); //define map & starting location
 
-    // iterate through sites in the db
-    for (site in siteList) {
-        currSite = siteList[site];
+    var markers = [];     //array of google map markers
+    var contents = [];    //content for the markers
+    var iWindows = [];    //windows to hold content for onclick
 
-        location = {
-            lat: parseFloat(currSite.latitude),
-            lng: parseFloat(currSite.longitude),
-        };
+    //iterate through sites in the db
+    for (var site in siteList) {
+        var currSite = siteList[site]
 
+        var location = {lat: parseFloat(currSite.latitude), lng: parseFloat(currSite.longitude)};
         contents[site] = '<div id="content">'+
-            '<div id="siteNotice">'+'</div>'+
-            '<h1 id="firstHeading" class="firstHeading">'+ currSite.name +
-            '</h1>'+
-            '<div id="bodyContent">' + '<p>' + currSite.description +'</p>' +
-            '<p>Site: <a href=' + currSite.link+ '>Current ' + currSite.name +
-            ' Projects</a></p>'+
-            '</div>'+
-            '</div>';
+          '<div id="siteNotice">'+'</div>'+'<h1 id="firstHeading" class="firstHeading">'+ currSite.name + '</h1>'+
+          '<div id="bodyContent">' + '<p>' + currSite.description +'</p>' +
+          '<p>Site: <a href=' + currSite.link+  '>Current ' + currSite.name + ' Projects</a></p>'+
+          '</div>'+
+          '</div>';
 
         markers[site] = new google.maps.Marker({
-            position: location,
-            map: map,
-            title: 'Energy Hill',
-            animation: google.maps.Animation.DROP,
+          position: location,
+          map: map,
+          title: 'Energy Hill',
+          animation: google.maps.Animation.DROP
         });
         markers[site].index = site;
 
         iWindows[site] = new google.maps.InfoWindow({
-            content: contents[site],
-            pixelOffset: new google.maps.Size(0, -20),
+          content: contents[site],
+          pixelOffset: new google.maps.Size(0,-20)
         });
 
-        google.maps.event.addListener(markers[site], 'click', function() {
-            for (i in iWindows) {
-                iWindows[i].close();
+        google.maps.event.addListener(markers[site], 'click', function () {
+            for (var i in iWindows){
+                iWindows[i].close()
                 markers[i].setAnimation(null);
             }
-            iWindows[this.index].open(map, markers[this.index]);
+            iWindows[this.index].open(map,markers[this.index]);
             map.panTo(markers[this.index].getPosition());
             markers[this.index].setAnimation(google.maps.Animation.BOUNCE);
         });
-    }
+  }
 
-    // close info windows on click off
-    google.maps.event.addListener(map, 'click', function() {
-        for (i in iWindows) {
-            iWindows[i].close();
-            markers[i].setAnimation(null);
-        }
-    });
+  //close info windows on click off
+  google.maps.event.addListener(map, 'click', function() {
+    for (var i in iWindows){
+      iWindows[i].close();
+      markers[i].setAnimation(null);
+    }
+  });
 }
