18e7745b64136185e791d340152ac0ff61f1ef56
jvoves@sqad.com-at-202332648106 <jvoves@sqad.com>
2018-02-01 19:58:45 -0500

    fixed Ajax Problem

--- web/common/db.js
+++ web/common/db.js
@@ -2,25 +2,16 @@
 function readDB(readTable, condition, fields){
 	var data = [];
 
-	$.ajax({
+	return $.ajax({
 	  type: 'GET',
-	  url: '../common/php/db.php',
+	  url: 'common/php/db.php',
 	  data: {
 	  	table: readTable,
 	  	condition: condition,
 	  	fields: fields
 	  },
 	  contentType: 'application/json',
-	  async: false,  //important. Makes sure init map waits till this is done.
-	  success: function(json) {
-	    $.each(JSON.parse(json), function(key,value) {
-	      data.push(value); //value = json object for site
-	    });
-	    console.log("Sucessfully Read Sites from DB");
-	  },
-	  error: function() {
-	    alert('Error Reading from DB');
-	  }
-	  });
-	  return data;
+	  })
+
 };
+
