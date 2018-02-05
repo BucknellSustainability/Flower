b23c0ead230cf7c4de57ac98ffae02c25884f1f5
Thomas Ficcadenti <thomasficcadenti@wifi171-195.bucknell.edu>
2018-02-04 23:30:42 -0500

    gitignore changed to ignore build files

--- web/control-panel/build.sh
+++ web/control-panel/build.sh
@@ -1,25 +1,12 @@
 #!/bin/bash
 # A script for building react source code into a production environment
 
-module load node #loads node & npm if not loaded already
+cd codebase/control-panel  #open development folder
 
-#check for dependencies & builds the control-panel
-cd ./codebase/control-panel/
-npm install
-npm run-script build
-cd ../../
+npm run build              #build the application
 
-#copy build files into web/control-panel
-cp -R ./codebase/control-panel/build/* .
+cp -R build/* ../../       #copy files from build folder into production folder
 
-#change permissions so the browser can see the relevent files
-chmod o=r index.html
-chmod o=r service-worker.js
-chmod o=r favicon.ico
-
-#makes files readable
-chmod -R o=r ./static 
-
-#makes files within folders accessible
-chmod o=rX ./static 
-chmod -R o=rX ./static/
+cd ../../                  #change directory back into production folder & set proper permissions
+chmod o=r ./*
+chmod o=r static/*
