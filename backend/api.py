4d4a817caff6ff5575de0b002b55a43f53ef9076
Ben Matase <bdm015@bucknell.edu>
2018-02-07 11:16:12 -0500

    Added profile getter

--- backend/api.py
+++ backend/api.py
@@ -1,5 +1,5 @@
-# export FLASK_DEBUG=1
-# export FLASK_APP=hello.py
+# export FLASK_DEBUG=0
+# export FLASK_APP=api.py
 # flask run
 
 import json
@@ -76,9 +76,10 @@ def modify():
 def get_profile():
     # TODO: validate that params are correct and sanitize inputs
     # validate userid
-    user_id = validate_user(request.args.get('id_token'))
+    # user_id = validate_user(request.args.get('id_token'))
 
-    return construct_profile_json(user_id)
+    # return construct_profile_json(user_id)
+    return construct_profile_json(request.args.get('userid'))
 
 def construct_profile_json(user_id):
     # fetch user info
@@ -94,44 +95,102 @@ def construct_profile_json(user_id):
     )
     projects = exec_query(projects_sql)
 
-    # TODO: figure out how to get just the project ids
     # use projects info to fetch devices
-    project_ids = projects
+    project_ids = [project['projectId'] for project in projects]
 
     # TODO: test if this "IN" thing is correct
     #fetch_device
     devices_sql = 'SELECT * FROM device WHERE projectId IN {}'.format(
-        # TODO: format project_ids from python list into mysql list
-        project_ids
+        str(tuple(project_ids))
     )
 
     devices = exec_query(devices_sql)
 
     # TODO: figure out how to get just the device ids
-    device_ids = device
+    device_ids = [device['deviceId'] for device in devices]
 
     # use devices info to fetch sensors
     sensors_sql = 'SELECT * FROM sensor WHERE deviceId IN {}'.format(
         # TODO: format device_ids from python list into mysql list
-        device_ids
+        str(tuple(device_ids))
     )
 
+    sensors = exec_query(sensors_sql)
+
+    sensors_dict = {}
+    devices_dict = {}
+    project_list = []
     # throw all data into dictionary to be converted to JSON
-    profile_dict = {}
+    for sensor in sensors:
+        # construct sensor dict
+        sensor_dict = {
+            'id': sensor['sensorId'],
+            'name': sensor['name']
+        }
+
+        device_id = sensor['device_id'];
+
+        # add to dict (if doesn't exist, create list)
+        if device_id not in sensors_dict.keys():
+            sensors_dict[device_id] = [sensor_dict]
+        else:
+            sensors_dict[device_id].append(sensor_dict)
+
+    for device in devices:
+        # construct device dict
+        device_dict = {
+            'id': device['deviceId'],
+            'name': device['name'],
+            'sensors': sensors_dict[device['deviceId']]
+        }
+
+        project_id = device['projectId']
+
+         # add to dict (if doesn't exist, create list)
+        if project_id not in devices_dict.keys():
+            devices_dict[project_id] = [device_dict]
+        else:
+            devices_dict[project_id].append(device_dict)
+
+    for project in projects:
+        # construct project dict
+        project_dict = {
+            'id': project['projectId'],
+            'name': project['name'],
+            'email': project['email'],
+            'alerts': project['alerts'],
+            'devices': devices_dict[project['projectId']]
+        }
+
+        project_list.append(project_dict)
+
+    profile_dict = {
+        'name': user_data['name']
+        'projects': project_list
+    }
 
     # return result as string
-    return json.dumps(profile_dict)
+    # return json.dumps(profile_dict)
+    return 'yay'
 
 def exec_query(sql_string):
     cursor = mysql.get_db().cursor()
+    data = None
     try:
         # Execute the SQL command
         cursor.execute(sql_string)
         # Fetch all the rows in a list of lists.
         data = cursor.fetchall()
-        return str(data)
     except:
         print("Error: unable o fetch data")
+
+    # Get column names
+    column_names = [column_info[0] for column_info in cursor.description]
+    formatted_data = []
+    for row in data:
+        formatted_data.append(dict(zip(column_names, row)))
+
+    return formatted_data
 
 
 def validate_user(id_token):
