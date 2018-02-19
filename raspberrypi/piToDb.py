5e2e4d7502db572776da5d0724f99e55f9b43831
Phlosioneer <mattmdrr2@gmail.com>
2018-02-19 18:14:59 -0500

    Progress with the piToDb part.

--- raspberrypi/piToDb.py
+++ raspberrypi/piToDb.py
@@ -21,50 +21,95 @@ def database_main(master_queue):
 		except Exception as e:
 			print(e)
 
-def database_loop(master_queue):
-	# Connect to the database.
-	connection = connectToDB()
+def get_all_readings(master_queue):
+	readings = []
+	try:
+		while True:
+			reading = master_queue.get_nowait()
+			readings.append(reading)
+	except queue.Empty:
+		pass
+	return readings
 
-	while True:
-		# Get all the stuff in the queue.
-		readings = []
-		try:
-			while True:
-				reading = master_queue.get_nowait()
-				readings.append(reading)
-		except queue.Empty:
-			pass
+# Takes in the full response of an arduino, and splits it into
+# an entry for each sensor returned by the arduino.
+#
+# Returns each sensor's value in (sensor_id, time, value) tuples.
+def process_reading(reading, connection):
+	# Get the arduino's DB ID.
+	device_id = get_arduino_dbid(connection, reading.getId())
 
-		# Process each reading.
-		data_to_push = []
-		for reading in readings:
-			# Get the arduino's DB ID.
-			device_id = get_arduino_dbid(connection, reading.getId())
+	# Get the data from the arduino.
+	json = reading.getData()
 
-			# Get the data from the arduino.
-			json = reading.getData()
+	# TODO: Check that the project name matches the one in the database.
 
-			# TODO: Check that the project name matches the one in the database.
+	# Iterate over every sensor value.
+	# TODO: Exclude the project name field in the json structure.
+	for name, value in json:
+		# Find the sensor ID.
+		sensor_id = get_sensor_dbid(connection, device_id, name)
 
-			# Iterate over every sensor value.
-			# TODO: Exclude the project name field in the json structure.
-			for name, value in json:
-				# Find the sensor ID.
-				sensor_id = get_sensor_id(connection, device_id, name)
+		return (sensor_id, reading.getTime(), value)
+
+def do_sensor_insert(cursor, sensor_id, time, value):
+	command_template = "INSERT INTO energyhill.data (sensorId, dateTime, value) VALUES (%s, FROM_UNIXTIME(%s), %s)"
 	
-				data_to_push.append((sensor_id, reading.getTime(), value))
+	assert(isinstance(sensor_id, int))
+	#assert(isinstance(time, DateTime)) # TODO: Fix this
+	assert(isinstance(value, float))
+	
+	command = command_template % (str(sensor), str(time), str(value))
+	do_sql(cursor, command)
 
-		# Send the readings in bulk.
-		with connection.cursor() as cursor:
-			command_template = "INSERT INTO energyhill.data (sensorId, dateTime, value) VALUES (%s, FROM_UNIXTIME(%s), %s)"
-			for sensor, time, value in data_to_push:
-				command = command_template % (str(sensor), str(time), str(value))
-				cursor.execute(command)
-				print(command)
+def do_sql(cursor, command):
+	print(command)
+	cursor.execute(command)
 
-			connection.commit()
+# TODO: If there is an error at any point after get_all_readings(), 
+# the data is lost!
+def database_loop(master_queue):
+	connection = None
+	try:
+		# Connect to the database.
+		connection = connectToDB()
 
+		while True:
+			# Get all the stuff in the queue.
+			readings = get_all_readings(master_queue)
 
+			# Process each reading.
+			data_to_push = []
+			for reading in readings:
+				data_to_push.append(process_reading(connection, reading))
+
+			# Send the readings in bulk.
+			with connection.cursor() as cursor:
+				for sensor, time, value in data_to_push:
+					do_sensor_insert(cursor, sensor, time, value)
+
+				connection.commit()
+	finally:
+		# Do cleanup if something goes wrong.
+		connection.close()
+
+def do_select_arduino(connection, cursor, id_string):
+	# Defensive escape, shouldn't ever be an issue.
+	id_string_escaped = cursor.escape(id_string)
+	
+	command = "SELECT deviceId FROM energyhill.device WHERE hardwareId IS %s" % id_string_escaped
+	do_sql(cursor, command)
+	connection.commit()
+		
+	# fetchall will return an array of dictionaries.
+	rows = cursor.fetchall()
+	return rows
+
+def do_insert_arduino(connection, cursor, id_string):
+	# Create a new entry for this board.
+	command = "INSERT INTO energyhill.device (deviceId) VALUES (%s)" % id_string_escaped
+	do_sql(cursor, command)
+	connection.commit()
 
 # TODO: Document this
 cached_arduino_ids = {}
@@ -73,28 +118,25 @@ def get_arduino_dbid(connection, id_string):
 		return cached_arduino_ids[id_string]
 
 	with connection.cursor(cursors.DictCursor) as cursor:
-		command = "SELECT deviceId FROM energyhill.device WHERE hardwareId IS %s" % id_string
-		cursor.execute(command)
-		connection.commit()
-
 		# fetchall will return an array of dictionaries.
-		rows = cursor.fetchall()
+		rows = do_select_arduino(connection, cursor, id_string) 
 		
 		if len(rows) == 0:
-			# Create a new entry for this board.
-			command = "INSERT INTO energyhill.device (deviceId) VALUES (%s)" % id_string
-			cursor.execute(command)
-			connection.commit()
-
+			do_insert_arduino(connection, cursor, id_string)
+			
 			# Try again.
-			return get_arduino_dbid(connection, id_string)
-
-		elif len(rows) == 1:
+			rows = do_select_arduino(connection, cursor, id_string)
+			
+			if len(rows) == 0:
+				raise Exception("Unable to insert arduino into the database.")
+		
+		# We now have rows from the server. Check that we only found ONE entry!
+		if len(rows) == 1:
 			arduino_id = cursor.fetchall()[0]["deviceId"]
 			cached_arduino_ids[id_string] = arduino_id
 			return arduino_id
 		else:
-			raise "Found multiple rows with hardwareId %s" % id_string
+			raise Exception("Found multiple rows with hardwareId %s" % id_string)
 		
 
 
@@ -102,15 +144,15 @@ def connectToDB():
 	try:
 		with open('../config.json', 'r') as f:
 			config = json.load(f)
-	except ValueError as e:
+	except ValueError:
 		# Catch and re-raise with a better error message.
-		raise type(e)('Error while parsing json file "../config.json": ' + e.message)
+		raise Exception('Error while parsing json file "../config.json"')
 
 	connection = pymysql.connect(host=config['DB_URL'], user=config['DB_USERNAME'], password=config['DB_PASSWORD'], db=config['DB_NAME'])
 	return connection
 
 
-
+# Old server code.
 def pushToDB(connection, number, sensor):
 	"""connection is the I/O object with the database. number is the number to insert, sensor is the sensor ID number."""
 	# Check that number and sensor are both integers, and not strings.
