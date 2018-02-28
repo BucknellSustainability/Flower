ad5a4e96f65f5b6f94866c8ad982f80970ffbf4a
Tom <tommy.ficcadenti@gmail.com>
2018-02-27 22:40:28 -0500

    got google authorization working finnaly

--- web/control-panel/codebase/control-panel/src/App.js
+++ web/control-panel/codebase/control-panel/src/App.js
@@ -5,20 +5,58 @@ import {Dashboard} from './dashboard/Dashboard.js'
 import {DashboardHeader} from './dashboard/DashboardHeader.js'
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      researcher: undefined,
+      permission: true
+    }
+  }
+
   render() {
     const dashboard = <Dashboard />
     const dashboardHeader = <DashboardHeader />
-    return (
-      <div className="App">
-        <header className="App-header">
-          {dashboardHeader}
 
-        </header>
+    if (this.state.permission == false) { //================= PERMISSION DENIED
+      return (
+        <div className="App">
+          <header className="App-header">
+            {dashboardHeader}
 
-        {dashboard}
-        
-      </div>
-    );
+          </header>
+
+          <h1 className="App-title">You do not have permission to view this page!</h1>
+
+        </div>
+      );//
+    } else { //============================================ PROFILE STILL LOADING
+      if (typeof this.state.researcher == 'undefined') {
+        return (
+          <div className="App">
+            <header className="App-header">
+              {dashboardHeader}
+
+            </header>
+
+            <h1 className="App-title">Loading...</h1>
+
+          </div>
+        );
+      } else { //========================================== PERMISSION GRANTED AND PROFILE LOADED
+        return (
+          <div className="App">
+            <header className="App-header">
+              {dashboardHeader}
+
+            </header>
+
+            {dashboard}
+
+          </div>
+        );
+      }
+    }
   }
 }
 
