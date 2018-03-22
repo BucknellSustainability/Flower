74ab9060fcd235f154d7207f7a81ea0d142f1fb1
Tom <tommy.ficcadenti@gmail.com>
2018-03-22 01:04:32 -0400

    "google login lifestyle complete"

--- web/control-panel/codebase/control-panel/src/dashboard/DashboardHeader.js
+++ web/control-panel/codebase/control-panel/src/dashboard/DashboardHeader.js
@@ -4,7 +4,29 @@ import '../fonts.css'
 import {Navbar} from 'react-bootstrap/lib/'
 
 export class DashboardHeader extends Component {
+  constructor() {
+    super();
+    this.signOut = this.signOut.bind(this);
+  }
+
+  signOut = () => {
+    var auth2 = this.props.gapi.auth2.getAuthInstance();
+
+    auth2.signOut().then(function () {
+      console.log('user was signed out');
+    });
+
+    this.props.signOut();
+  }
+
   render() {
+    let signedInAs = null;
+    let btnSignOut = null;
+    if (this.props.email != '') {
+      btnSignOut = <button type="button" className="btn btn-default navbar-btn" onClick={this.signOut}>Sign Out</button>
+      signedInAs = "Signed in as:"
+    }
+
     return (
       <div>
         <Navbar className="App-Nav">
@@ -18,7 +40,12 @@ export class DashboardHeader extends Component {
               <Navbar.Text>
                     <h2 className="App-title bold concert">Energy Hill Dashboard</h2>
               </Navbar.Text>
-              <Navbar.Text className="App-sub-text concert" pullRight>Signed in as: <Navbar.Link className="App-sub-text" href="#">jav017@bucknell.edu</Navbar.Link></Navbar.Text>
+              <Navbar.Text className="App-sub-text concert" pullRight>{signedInAs}
+                <Navbar.Link className="App-sub-text" href="#">
+                  {" " + this.props.email + " "}
+                </Navbar.Link>
+                {btnSignOut}
+              </Navbar.Text>
             </Navbar.Collapse>
           </Navbar>
       </div>
