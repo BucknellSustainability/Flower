bfbf074355e712c82ff7987825f1df34a8cf5bda
Linux Server <energyhill@bucknell.edu>
2018-04-24 22:54:37 -0400

    Fixed up some things with the setup scripts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,17 +1,22 @@
 chmod +x ./*
 
+echo "Please enter the information:"
 module load python/3.6
 pip install --user --upgrade getpass
 python setup.py
 
 # start flask
+echo "starting flask server"
 ./restart_flask.sh
 
 # build react
+echo "building react pages"
 ./build.sh
 
 # setup alert/aggregator
+echo "starting alert/aggregator"
 ./crontab.sh
 
 # change permissions 
+echo "allowing all pages to be accessed"
 ./change_permissions.sh
